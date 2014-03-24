4ebac72ab140da8f064ebf2347df8c765cf7169d
amber <amber.houle3@gmail.com>
2014-03-24 13:30:21 -0500

    [103] (Amber, Matthew) Added edit and update functionality to journey controller

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -7,7 +7,7 @@ class JourneysController < ApplicationController
     if @journey.save
       redirect_to value_proposition_path(params[:value_proposition_id])
     else
-       render action: 'new'
+      render action: 'new'
     end
   end
 
@@ -16,11 +16,28 @@ class JourneysController < ApplicationController
     @value_proposition_id = params[:value_proposition_id]
   end
 
+  def edit
+    @journey = load_journey_to_update
+  end
+
+  def update
+      if load_journey_to_update.update(journey_params)
+        redirect_to value_proposition_path(params[:value_proposition_id])
+      else
+        render action: 'edit'
+      end
+  end
+
   def journey_params
     params.require(:journey).permit(:title, :value_proposition_id)
   end
+
   def load_value_proposition
     @value_proposition = ValueProposition.find(params[:value_proposition_id])
+  end
+
+  def load_journey_to_update
+    @value_proposition.journeys.find(params[:id])
   end
 
 end
--- spec/controllers/journeys_controller_spec.rb
+++ spec/controllers/journeys_controller_spec.rb
@@ -1,19 +1,23 @@
 require 'spec_helper'
 
 describe JourneysController do
-  let(:mock_journey) { mock_model(Journey, title: "JourneyTitle", value_proposition_id: "0") }
+  let(:mock_journey) { mock_model(Journey, id: "0", title: "JourneyTitle", value_proposition_id: "0") }
   let(:mock_value_proposition) { mock_model(ValueProposition, name: "VpName", description: "description", value_proposition_category: "category")}
-  let(:valid_attributes) { { title: "JourneyTitle", value_proposition_id: "0"} }
   let(:value_proposition_id) { "0" }
+  let(:valid_attributes) { { title: "JourneyTitle", value_proposition_id: value_proposition_id } }
 
   before do
+    @admin_user = FactoryGirl.create(:user, :admin)
     Journey.stub(:new).and_return(mock_journey)
     ValueProposition.stub(:find).with(value_proposition_id).and_return(mock_value_proposition)
-    journeys_proxy = double("association proxy", {"new" => Journey.new})
-    mock_value_proposition.stub(:journeys).and_return(journeys_proxy)
   end
 
   describe "POST create" do
+    before do
+      journeys_proxy = double("association proxy", {"new" => Journey.new})
+      mock_value_proposition.stub(:journeys).and_return(journeys_proxy)
+    end
+
     it "calls save on the journey" do
       mock_journey.should_receive(:save)
       post :create, { value_proposition_id: value_proposition_id,
@@ -38,6 +42,10 @@ describe JourneysController do
   end
 
   describe "GET new" do
+    before do
+      journeys_proxy = double("association proxy", {"new" => Journey.new})
+      mock_value_proposition.stub(:journeys).and_return(journeys_proxy)
+    end
     it "loads a new journey" do
       Journey.should_receive(:new)
       get :new, { value_proposition_id: value_proposition_id}
@@ -49,8 +57,57 @@ describe JourneysController do
     end
 
     it "loads the value proposition id" do
-      get :new, { value_proposition_id: 0}
+      get :new, { value_proposition_id: value_proposition_id}
       assigns(:value_proposition_id).should eq value_proposition_id
+    end
+  end
+
+  describe "GET edit" do
+    it "assigns @journey to journey we want to edit" do
+      journey_id_from_param = "0"
+      mock_value_proposition.stub_chain(:journeys, :find).with(journey_id_from_param).and_return(mock_journey)
+      get :edit, { value_proposition_id: value_proposition_id, id: mock_journey.id}
+      assigns(:journey).should eq mock_journey
+    end
+  end
+
+  describe 'PUT update' do
+    context 'as an admin user' do
+      before do
+        #controller.stub(:current_user).and_return(@admin_user)
+        #controller.stub_chain(:current_user, :present).and_return(true)
+        #controller.stub_chain(:current_user, :can_edit_and_delete_resource).with(anything()).and_return(true)
+        journey_id_from_param = "0"
+        mock_value_proposition.stub_chain(:journeys, :find).with(journey_id_from_param).and_return(mock_journey)
+      end
+
+      it "calls update on journey" do
+        mock_journey.should_receive(:update)
+        put :update, { value_proposition_id: value_proposition_id,
+                       id: mock_journey.id,
+                       journey: valid_attributes,
+                       }
+
+      end
+      describe "successful journey update" do
+        it "should redirect to value proposition page" do
+          mock_journey.stub(:update).with(anything()).and_return(true)
+          put :update, {  value_proposition_id: value_proposition_id,
+                          id: mock_journey.id,
+                          journey: valid_attributes}
+          response.should redirect_to(value_proposition_path(value_proposition_id))
+        end
+      end
+      describe "journey update failure" do
+        it "should render the edit journey page" do
+          mock_journey.stub(:update).with(anything()).and_return(false)
+          put :update, { value_proposition_id: value_proposition_id,
+                         id: mock_journey.id,
+                         journey: valid_attributes}
+          response.should render_template("edit")
+
+        end
+      end
     end
   end
 end
