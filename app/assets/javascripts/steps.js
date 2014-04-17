45794b948202f7430e6e5eb3938725ec9540fba0
amber <amber.houle3@gmail.com>
2014-04-17 10:42:22 -0500

    (Amber) Nested adding resource inside add a step, and appended add a step to the bottom of the form

--- app/assets/javascripts/steps.js
+++ app/assets/javascripts/steps.js
@@ -1,20 +1,24 @@
 $('document').ready(function() {
   $('#step-form').hide();
+  $('#resource-btn').hide();
 
   $('#step-btn').click(function(){
     if($('#step-form').is(':hidden')){
       $('#step-form').slideDown('slow');
+      $('#resource-btn').slideDown('slow');
       $('#step-btn').text('Done Adding Step');
     }
     else{
       $('#step-form').slideUp('slow');
+      $('#resource-btn').slideUp('slow');
+      $('#resource-form').slideUp('slow');
       $('#step-btn').text('Edit Step');
     }
   });
 
   $('.add-new-step').click(function(){
     var stepForm = $('.add-step').clone();
-    $('#resource-form').append(stepForm);
+    $('#step-btn').after(stepForm);
   });
 });
 
